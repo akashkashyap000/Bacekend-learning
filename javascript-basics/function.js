9c28d3a156d8af01887a6dda8b380fa6d24f781c
akashkashyap000 <akashkashyap2799@gmail.com>
2025-08-25 13:04:07 +0530

    updated javascript-basics

--- javascript-basics/function.js
+++ javascript-basics/function.js
@@ -1,1 +1,10 @@
-hello
+
+// Normal Function
+function add(a, b) {
+  return a + b;
+}
+console.log("Add:", add(5, 3));
+
+// Arrow Function
+const multiply = (a, b) => a * b;
+console.log("Multiply:", multiply(4, 2));
